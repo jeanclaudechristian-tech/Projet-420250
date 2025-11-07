cfdeefd6c45754bf462b52c61eefb1427a336304
Kouassi <robertokouassi21@gmail.com>
2025-11-07 15:19:14 -0500

    Création de la structure HTML de la page d'accueil

---
+++ header.php
@@ -0,0 +1,9 @@
+<?php /* 🔹 En-tête du site Tuto+ */ ?>
+
+<header>
+    <nav>
+        <a href="/index.php">Accueil</a>
+        <a href="/index.php#services">Services</a>
+        <a href="/index.php#contact">Contact</a>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tuto+ — Collège Ahuntsic</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+    <?php include __DIR__ . '/header.php'; ?>
+    
+    <main>
+       
+
+    </main>
+    <?php include __DIR__ . '/footer.php'; ?>
+
+</body>
+</html>

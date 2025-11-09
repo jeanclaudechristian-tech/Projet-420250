b11dc954858ae3f4cd2affbb836ea09545fe9c1d
Christian <Chris27@email.com>
2025-11-08 19:47:43 -0500

    Nettoyage complet de la branche

--- header.php
+++
@@ -1,9 +0,0 @@
-<?php /* 🔹 En-tête du site Tuto+ */ ?>
-
-<header>
-    <nav>
-        <a href="/index.php">Accueil</a>
-        <a href="/index.php#services">Services</a>
-        <a href="/index.php#contact">Contact</a>
-    </nav>
-</header>
